d4a5544f763a07431ccdb78724d9e2723cf0a77a
Kim Crayton <kmcrayton7@yahoo.com>
2015-09-20 22:55:16 -0400

    Added wiring for the second partial

--- js/app.js
+++ js/app.js
@@ -9,7 +9,11 @@ myApp.config(['$routeProvider', function($routeProvider) {
 		templateUrl: 'partials/list.html',
 		controller: 'ListController'
 	}).
+	when('/details/:itemId', {
+		templateUrl: 'partials/details.html',
+		controller: 'DetailsController'
+	}).
 	otherwise({
 		redirectTo: '/list'
-	})
+	});
 }]);
--- js/controllers.js
+++ js/controllers.js
@@ -1,8 +1,15 @@
-var myApp = angular.module('artistControllers', []);
+var artistControllers = angular.module('artistControllers', []);
 
-myApp.controller('ListController', ['$scope', '$http', function($scope, $http) {
+artistControllers.controller('ListController', ['$scope', '$http', function($scope, $http) {
 	$http.get('js/data.json').success(function(data) {
 		$scope.artists = data;
 		$scope.artistOrder = 'name';
+	});  
+}]);
+
+artistControllers.controller('DetailsController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
+	$http.get('js/data.json').success(function(data) {
+		$scope.artists = data;
+		$scope.whichItem = $routeParams.itemId;
 	});  
 }]);
