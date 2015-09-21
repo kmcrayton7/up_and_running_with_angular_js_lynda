8ca56eab0ddd93ec97551f9e18ec9a3772dbae4e
Kim Crayton <kmcrayton7@yahoo.com>
2015-09-20 22:16:56 -0400

    Set up deep linking

--- js/controllers.js
+++ js/controllers.js
@@ -1,6 +1,6 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('artistControllers', []);
 
-myApp.controller('MyController', ['$scope', '$http', function($scope, $http) {
+myApp.controller('ListController', ['$scope', '$http', function($scope, $http) {
 	$http.get('js/data.json').success(function(data) {
 		$scope.artists = data;
 		$scope.artistOrder = 'name';
